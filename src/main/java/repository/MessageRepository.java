33f27c7d3b81a8445d168a7fe2f98e8e1638dcf1
Shashank <shashankps2903@gmail.com>
2026-06-24 12:54:16 +0530

    add some additonals

--- src/main/java/repository/MessageRepository.java
+++ src/main/java/repository/MessageRepository.java
@@ -1,11 +1,12 @@
 package repository;
 
 import entity.Message;
-import entity.Notice;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface MessageRepository extends JpaRepository<Message,Long> {
 
     List<Message> findTop3ByOrderByIdDesc();
--- src/main/java/repository/NoticeRepository.java
+++ src/main/java/repository/NoticeRepository.java
@@ -2,9 +2,10 @@ package repository;
 
 import entity.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
     List<Notice> findTop3ByOrderByIdDesc();
